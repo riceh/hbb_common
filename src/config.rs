400fbaac1a8d4bae281d992074b6a9174ec9024a
riceh <34214565+riceh@users.noreply.github.com>
2025-04-21 00:58:52 +0800

    Update config.rs
    修改默认服务器和key

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.miaoge.fun"];
+pub const RS_PUB_KEY: &str = "D95yHjplgnyRixe8WxG3OCoV3eleet4EfBdoDRDoxrE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
